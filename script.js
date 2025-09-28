dbd13bb6d567eed8c2fd7a65460f993cde08c46f
LesMrCode <mhonelesleyeric@gmail.com>
2025-09-28 18:09:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Code for Time//
+function updateClock24() {
+    const now = new Date();
+
+    let hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById("clock").textContent = timeString;
+
+    // Code for Date//
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    const dayName = days[now.getDay()];
+    const monthName = months[now.getMonth()];
+    const day = now.getDate();
+    const year = now.getFullYear();
+
+    const dateString = `${dayName}, ${monthName} ${day}, ${year}`;
+
+    document.getElementById("date").textContent = dateString;
+}
+
+
+
+
+
+
+updateClock24();
+setInterval(updateClock24, 1000);
+
+
+
